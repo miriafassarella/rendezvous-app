daae6f6deb54f0f16951887fa15175c2be5aff00
Miria Fassarela <miriafassarella@gmail.com>
2026-03-26 12:32:04 -0400

    feat(security): add JwtUtil for token generation and validation

--- rendezvous-app/src/main/java/com/rendezvous/security/JwtUtil.java
+++ rendezvous-app/src/main/java/com/rendezvous/security/JwtUtil.java
@@ -1,4 +1,60 @@
 package com.rendezvous.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+
+    private SecretKey getSiningKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSiningKey())
+                .compact();
+    }
+
+    public String extractEmail(String token){
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token){
+        try{
+            extractClaims(token);
+            return true;
+        }catch (JwtException | IllegalArgumentException e){
+            return false;
+        }
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSiningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
