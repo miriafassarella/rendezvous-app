6f19bac56f1cd00d6b1a54f1629f58c372b4f942
Miria Fassarela <miriafassarella@gmail.com>
2026-03-27 15:16:16 -0400

    feat(security): add JwtFilter, UserDetailsServiceImpl and update SecurityConfig

--- rendezvous-app/src/main/java/com/rendezvous/security/JwtFilter.java
+++ rendezvous-app/src/main/java/com/rendezvous/security/JwtFilter.java
@@ -1,4 +1,48 @@
 package com.rendezvous.security;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private JwtUtil jwtUtil;
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    public JwtFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService){
+
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        String token = null;
+        String email = null;
+
+        // 1. Extrai o token do header
+        if (authHeader != null && authHeader.startsWith("Bearer ")){
+            token = authHeader.substring(7);
+            email = jwtUtil.extractEmail(token);
+        }
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            if (jwtUtil.isTokenValid(token) && !jwtUtil.isTokenExpired(token)){
+                UserDetails userDetails; //a concluir
+            }
+        }
+
+    }
 }
