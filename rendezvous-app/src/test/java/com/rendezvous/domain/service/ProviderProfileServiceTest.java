9443683f9091ee7ef00bcb8db323447666ec3ed8
Miria Fassarela <miriafassarella@gmail.com>
2026-05-05 15:46:49 -0400

    test: add tests for provider not found and delete provider without appointments

--- rendezvous-app/src/test/java/com/rendezvous/domain/service/ProviderProfileServiceTest.java
+++ rendezvous-app/src/test/java/com/rendezvous/domain/service/ProviderProfileServiceTest.java
@@ -2,6 +2,7 @@ package com.rendezvous.domain.service;
 import com.rendezvous.domain.model.ProviderProfile;
 import com.rendezvous.domain.repository.*;
 import com.rendezvous.dto.providerProfileDto.ProviderProfileResponseDTO;
+import com.rendezvous.exception.ProviderNotFoundException;
 import com.rendezvous.mapper.ProviderProfileMapper;
 import com.rendezvous.mapper.UserMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
@@ -65,5 +67,33 @@ class ProviderProfileServiceTest {
         List<ProviderProfileResponseDTO> result = providerProfileService.findProviderAll();
 
         assertThat(result).isEmpty();
+    }
+
+    @Test
+    void shouldReturnprovider_whenProviderExist(){
+        when(providerProfileRepositoy.findById(1L)).thenReturn(Optional.of(providerProfile));
+        when(providerProfileMapper.toResponseDTO(providerProfile)).thenReturn(providerProfileResponseDTO);
+
+        ProviderProfileResponseDTO result = providerProfileService.findById(1L);
+
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    void shouldThrowProviderNotFoundException_whenProviderNotFound(){
+        when(providerProfileRepositoy.findById(99L)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(()-> providerProfileService.findById(99L))
+                .isInstanceOf(ProviderNotFoundException.class);
+    }
+
+    @Test
+    void shouldDeleteClient_whenClientHasNoAppointments(){
+        when(providerProfileRepositoy.findById(1L)).thenReturn(Optional.of(providerProfile));
+       when(appointmentRepository.existsByProvider(providerProfile)).thenReturn(false);
+
+       providerProfileService.deleteProvider(1L);
+
+       verify(providerProfileRepositoy, times(1)).delete(providerProfile);
     }
 }
