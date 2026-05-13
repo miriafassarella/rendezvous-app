f62f49bee5639418f5e489c739180bf192345a2c
Miria Fassarela <miriafassarella@gmail.com>
2026-05-13 14:06:56 -0400

    remove Spring Security and JWT configuration for training

--- rendezvous-app/src/main/java/com/rendezvous/exception/GlobalExceptionHandler.java
+++ rendezvous-app/src/main/java/com/rendezvous/exception/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.rendezvous.exception;
 
-import com.rendezvous.dto.exceptionError.ErrorResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
